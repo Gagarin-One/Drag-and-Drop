d5892d902ca03482bc29ff754c600b0d2455b74f
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2022-05-30 22:27:03 +0900

    refactoring app component and add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Kanban from "./Kanban";
+import './App.scss'
 
-import './App.css';
 
 function App() {
   return (
-    
+    <div style={{ padding: '50px' }}>
+            <h1 style={{ marginBottom: '20px' }}>
+            Drag and drop
+            </h1>
+            <Kanban/>
+        </div>
   );
 }
 
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 export default App;
